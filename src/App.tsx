1bb413775c7d51d971877ca3d617dd184cb88a69
Rai Okushima <okushima.rai.github@gmail.com>
2026-08-12 20:03:25 +0900

    fix(layout): 仮で作成していたルート・メニューを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Routes, Route } from 'react-router'
 import Layout from './components/Layout'
 import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
 import Calculator from './pages/Calculator'
 
 function App() {
@@ -10,8 +8,6 @@ function App() {
     <Routes>
       <Route element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
         <Route path="calculator" element={<Calculator />} />
       </Route>
     </Routes>
